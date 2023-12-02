efcb95718e4299da8fd6768dd9db9dc3327d3546
Михајловић Бојана <83251325+wildbohana@users.noreply.github.com>
2023-12-02 16:29:44 +0100

    engine osnova
    flask api, postgresql (lokalan), modeli
    imam neku grešku sa učitavanjem modela, to trebam da popravim

---
+++ engine/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Setup
+app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/DRSBaza'
+db = SQLAlchemy(app)
+
+
+# Home
+@app.route("/")
+def home():
+    return "Home"
+
+
+# Test method
+@app.route("/members")
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]}
+
+
+# Initialize server
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+
+"""
+from models.User import User
+from models.Card import Card
+from models.Product import Product
+from models.Transaction import Transaction
+"""
